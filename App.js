ad288dc0dd7de45ebd00e53d27146b8a9de7f1fe
Jenny Lee <jennylee038@gmail.com>
2023-02-07 18:33:21 -0800

    implemented tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { Feather } from "@expo/vector-icons";
+import Home from "./Home";
+import History from "./History";
+import Schedule from "./Schedule";
+import Settings from "./Settings";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              return <Feather name="home" size={size} color={color} />;
+            } else if (route.name === "History") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            } else if (route.name === "Schedule") {
+              return <Feather name="calendar" size={size} color={color} />;
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-settings" : "ios-settings-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#28D8A1",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="History" component={History} />
+        <Tab.Screen name="Schedule" component={Schedule} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
