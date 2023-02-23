430fa38f59d1cd5421f41c1929615e9de6eec3c3
Grace Manning <gracebmanning@gmail.com>
2023-02-23 01:02:59 -0800

    basic pedometer functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,49 @@
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Pedometer } from "expo-sensors";
+//import { Permissions } from "@expo/config-plugins/build/android";
+//import { getAndroidManifestAsync } from "@expo/config-plugins/build/android/Paths";
 
 export default function Home() {
+  const [ PedometerAvailable, setPedometerAvailable ] = useState(""); // starts empty
+  const [ stepCount, setStepCount ] = useState(0); // starts at 0 steps
+  
+  useEffect(()=>{
+    subscribe(); // calls the subscribe function when app starts
+  }, [])
+
+  const subscribe = () => {
+    
+    Pedometer.watchStepCount((result)=>{
+      setStepCount(result.steps);
+    })
+
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        setPedometerAvailable(String(result)); // result = 1|0. String(result) = true|false.
+      },
+      (error) => {
+        setPedometerAvailable(error);
+      }
+    )
+
+    // Need to set pedometer permissions
+    /*
+    getAndroidManifestAsync((result) =>{
+      Permissions.addBlockedPermissions(result, ["android.permission.ACTIVITY_RECOGNITION"]);
+    });
+    */
+    
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.walkText}>
         Walk
         <Text style={styles.timeText}>Time</Text>
       </Text>
+      <Text>Is pedometer available on device?: {PedometerAvailable}</Text>
+      <Text>Steps: {stepCount}</Text>
     </View>
   );
 }
