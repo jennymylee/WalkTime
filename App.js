3249b37915eba1ed007a6510822a4fa79e19987a
Ye Min Aung <simonaung1495@gmail.com>
2023-02-24 13:26:03 -0800

    Added Local Notifications

--- App.js
+++ App.js
@@ -11,6 +11,12 @@ import Profile from "./screens/Profile";
 import LogIn from "./screens/LogIn";
 import SignUp from "./screens/SignUp";
 
+// for Notifications
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import {  Platform } from "react-native";
+
 const Tab = createBottomTabNavigator();
 const SettingsStack = createStackNavigator();
 const AuthStack = createStackNavigator();
@@ -63,7 +69,80 @@ function TabsInterface() {
   );
 }
 
+// Setting up notifications for the app
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
 export default function App() {
+  // Adding notification listener and subscriber
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => 
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current = 
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current = 
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+    
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+  
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        sound: true,
+        lightColor: "#FF231F7C",
+        lockscreenVisibility: Notifications.AndroidNotificationVisibility.PUBLIC,
+        bypassDnd: true,
+      });
+    }
+  
+    return token;
+  }
+
   return (
     <>
       <NavigationContainer>
