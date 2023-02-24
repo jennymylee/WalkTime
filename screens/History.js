07892f92014bd182671de71a3970e4f085ca26a4
Jenny Lee <jennylee038@gmail.com>
2023-02-24 01:44:18 -0800

    completed history ui with dummy log

--- screens/History.js
+++ screens/History.js
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import HistoryEntry from "../components/HistoryEntry";
 
 export default function History() {
+  // dummy history log: {date str: {time str: [distance in miles, time elapsed in seconds]}}
+  const history = [
+    {
+      date: "Monday, 1/30/2023",
+      data: [
+        { startTime: "12:56 PM", distance: 1.34, timeElapsed: 3400 },
+        { startTime: "3:00 PM", distance: 2.03, timeElapsed: 3600 },
+        { startTime: "5:00 PM", distance: 0.8, timeElapsed: 3500 },
+      ],
+    },
+    {
+      date: "Tuesday, 1/31/2023",
+      data: [
+        { startTime: "12:56 PM", distance: 1.34, timeElapsed: 3400 },
+        { startTime: "3:00 PM", distance: 2.03, timeElapsed: 3600 },
+        { startTime: "5:00 PM", distance: 0.8, timeElapsed: 3500 },
+        { startTime: "8:00 PM", distance: 0.98, timeElapsed: 3550 },
+      ],
+    },
+    {
+      date: "Wednesday, 2/1/2023",
+      data: [
+        { startTime: "12:56 PM", distance: 1.34, timeElapsed: 3400 },
+        { startTime: "3:00 PM", distance: 2.03, timeElapsed: 3600 },
+        { startTime: "5:00 PM", distance: 0.8, timeElapsed: 3500 },
+        { startTime: "8:00 PM", distance: 0.98, timeElapsed: 3550 },
+        { startTime: "8:00 PM", distance: 1.2, timeElapsed: 3850 },
+      ],
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text style={styles.walkText}>
-        Walk
-        <Text style={styles.historyText}>History</Text>
-      </Text>
+      <View style={styles.top}>
+        <Text style={styles.walkText}>
+          Walk
+          <Text style={styles.historyText}>History</Text>
+        </Text>
+        <Text style={styles.subheading}>A log of your most recent walks.</Text>
+      </View>
+      <FlatList
+        style={styles.log}
+        data={history}
+        renderItem={HistoryEntry}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -17,9 +57,6 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     backgroundColor: "#fff",
-    paddingTop: 50,
-    paddingLeft: 30,
-    paddingRight: 30,
   },
   walkText: {
     fontWeight: "bold",
@@ -27,6 +64,25 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   historyText: {
-    color: "#28D8A1",
+    color: "#fff",
+  },
+  top: {
+    paddingTop: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+    backgroundColor: "#28D8A1",
+  },
+  subheading: {
+    fontWeight: "bold",
+    fontSize: 17,
+    marginTop: 5,
+    marginBottom: 20,
+    color: "#fff",
+  },
+  log: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 15,
+    paddingBottom: 15,
   },
 });
