331f5025924aa7240da019951f4f5b03bf1215f3
Dingyi (Jaedyn) Chang <dingyichang2020@gmail.com>
2023-03-21 17:27:50 -0700

    fix: change new Map() to {}

--- models/weather-request.js
+++ models/weather-request.js
@@ -16,12 +16,12 @@ export async function getWeatherData() {
     ];
 
     let data_days = data.days;
-    const precipMap = new Map();
+    const precipMap = {};
     for (var i = 0; i < data_days.length - 1; i++) {
       let curr_day = data_days[i];
       let day_index = new Date(curr_day.datetimeEpoch * 1000).getDay();
       let day_word = days[day_index];
-      let hourMap = new Map();
+      let hourMap = {};
       for (var j = 0; j < curr_day.hours.length; j++) {
         let hourInt = parseInt(curr_day.hours[j].datetime.slice(0,2));
         hourMap.set(hourInt, curr_day.hours[j].precipprob);
